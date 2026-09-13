f4f5becad16860364f153e7a9bbfdf6ad59b021a
Alyssa Holland <2011hollanda@gmail.com>
2026-09-13 02:39:12 +0100

    updated timeout to 15 mins for extra large fics

--- src/app.py
+++ src/app.py
@@ -125,7 +125,7 @@ def worker():
             
             # Download file
             headers = {"User-Agent": "Mozilla/5.0"}
-            r = requests.get(download_url, headers=headers, stream=True, timeout=30)
+            r = requests.get(download_url, headers=headers, stream=True, timeout=900)
             r.raise_for_status()
             
             with open(filepath, 'wb') as f:
